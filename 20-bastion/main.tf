92b1c1ecc88732334740fe19e886f5694564bf5a
subbu <subbunandyala48@gmail.com>
2025-11-24 17:55:41 +0530

    jenkins

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bastion" {
   ami           = local.ami_id
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
 
