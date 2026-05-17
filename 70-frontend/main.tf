1eafa4ebc68ba7309b8f26ab796c1f02cb0334d4
subbu <subbunandyala48@gmail.com>
2026-05-17 17:35:33 +0530

    jenkins

--- 70-frontend/main.tf
+++ 70-frontend/main.tf
@@ -30,7 +30,7 @@ resource "aws_lb_listener" "ingress_alb" {
       message_body = "<h1>Hello, I am from ingress ALB using HTTPS</h1>"
       status_code  = "200"
     }
-  }
+  } 
 }
 
 resource "aws_route53_record" "ingress_alb" {
@@ -45,3 +45,37 @@ resource "aws_route53_record" "ingress_alb" {
   }
 }
 
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "${var.project}-${var.environment}-frontend" #roboshop-dev-catalogue
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 120
+  target_type = "ip"     #by default it takes "instances" keep target type as "IP ADDRESS"
+  health_check {
+    healthy_threshold = 2
+    interval = 5
+    matcher = "200-299"
+    path = "/"
+    port = 8080
+    timeout = 2
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.ingress_alb.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.environment}.${var.zone_name}"] # https://dev.subbuaws.site
+    }
+  }
+}
