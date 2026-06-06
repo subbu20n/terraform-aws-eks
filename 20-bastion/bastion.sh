278db364739b75261dcd6af1cc055975b6e1e2ae
subbu <subbunandyala48@gmail.com>
2026-06-06 22:47:39 +0530

    terraform-aws-eks

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -16,7 +16,7 @@ dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker 
-usermod -aG docker ec2-user
+usermod -aG docker ec2-user 
 
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
